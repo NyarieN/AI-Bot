aaa1b884aba995f88c7015608786cafd9d8a0b93
NyarieN <nyaradzondoro1@gmail.com>
2025-11-07 09:35:56 +0200

    updates

--- testapp.py
+++ testapp.py
@@ -111,5 +111,5 @@ def test_widget():
     return render_template("test.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
